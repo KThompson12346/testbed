04c12a05928ad951569a2f4cbb2ca702906f5200
KThompson <kt3223@gmail.com>
2019-04-29 17:56:27 +0100

    random colours code method
    to be used for the lines on the tkinter canvas

--- Controller/controller.py
+++ Controller/controller.py
@@ -8,6 +8,13 @@ class Controller:
             numbers_list.append(random.randint(1, up_to))
         return numbers_list
 
+    def random_colour_code():
+        hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+        colour_code = '#'
+        for i in range(0,6):
+            colour_code = colour_code + choice(hex_chars)
+        return colour_code
+
     def clear_list(self, list):
         list.clear()
         return list
